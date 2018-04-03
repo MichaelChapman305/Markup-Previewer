246ef1b30e9573d8d6b2de96a1567d5c7a1a8714
Michael A. Chapman <34172511+MichaelChapman305@users.noreply.github.com>
2018-04-03 14:25:18 -0400

    Fix spacing

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Output from './Output.jsx';
 
 class App extends Component{
   constructor(props) {
-  	super(props);
-  	this.state = {
+    super(props);
+    this.state = {
       text: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*'
     }
 
@@ -29,12 +29,12 @@ class App extends Component{
   }
 
   render() {
-  	return (
-  	  <div className="main">
-  	    <Markdown text={this.state.text} textChange={this.textChange} />
+    return (
+      <div className="main">
+        <Markdown text={this.state.text} textChange={this.textChange} />
         <Output markup={this.markup} />
-  	  </div>
-  	);
+      </div>
+    );
   }
 }
 
