ff740c4e39b3e057206a0b2412b897670c3ac621
Michael A. Chapman <34172511+MichaelChapman305@users.noreply.github.com>
2018-04-03 14:21:17 -0400

    Fix spacing

--- src/OutPut.jsx
+++ src/OutPut.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 import marked from 'marked';
 
 export default class Output extends Component {
-	render() {
-		return (
-			<div className="output-container">
-			  <h1>Output Preview</h1>
-			  <div className="output" dangerouslySetInnerHTML={this.props.markup()}></div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="output-container">
+        <h1>Output Preview</h1>
+        <div className="output" dangerouslySetInnerHTML={this.props.markup()}></div>
+      </div>
+    );
+  }
 }
 
 Output.propTypes = {
-	markup: PropTypes.func.isRequired
+  markup: PropTypes.func.isRequired
 }
