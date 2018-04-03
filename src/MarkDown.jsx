d70a18d00509c2db2fa3761d69c10c6b54cb06e1
Michael A. Chapman <34172511+MichaelChapman305@users.noreply.github.com>
2018-04-03 14:19:18 -0400

    Fix spacing

--- src/MarkDown.jsx
+++ src/MarkDown.jsx
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 export default class Markdown extends Component {
-	render() {
-		return (
-			<div className="mark-down-container">
-			  <h1>Markdown</h1>
-			  <textarea 
-				  className="mark-down-text" 
-				  rows="40" cols="75"
-				  onChange={this.props.textChange}
-				  defaultValue={this.props.text}>
-			  </textarea>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="mark-down-container">
+	<h1>Markdown</h1>
+	  <textarea 
+	    className="mark-down-text" 
+	    rows="40" cols="75"
+	    onChange={this.props.textChange}
+	    defaultValue={this.props.text}>
+	  </textarea>
+      </div>
+    );
+  }
 }
 
 Markdown.propTypes = {
-	text: PropTypes.string.isRequired,
-	textChange: PropTypes.func.isRequired
+  text: PropTypes.string.isRequired,
+  textChange: PropTypes.func.isRequired
 }
